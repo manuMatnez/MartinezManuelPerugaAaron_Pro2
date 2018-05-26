157a0544ee8418a67e607eb1cfcfc33833a24375
manuMatnez <mmartima285@alumnes.ub.edu>
2018-05-26 21:31:18 +0200

    testeando swing en netbeans

--- src/edu/ub/prog2/MartinezManuelPerugaAaron/vista/AplicacioUB4.java
+++ src/edu/ub/prog2/MartinezManuelPerugaAaron/vista/AplicacioUB4.java
@@ -52,24 +52,181 @@ public class AplicacioUB4 extends JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        cmbAlbum = new javax.swing.JComboBox<>();
+        scpAlbums = new javax.swing.JScrollPane();
+        lstAlbums = new javax.swing.JList<>();
+        scpBiblioteca = new javax.swing.JScrollPane();
+        lstBiblioteca = new javax.swing.JList<>();
+        lblBiblioteca = new javax.swing.JLabel();
+        lblAlbums = new javax.swing.JLabel();
+        cbRepAleatoria = new javax.swing.JCheckBox();
+        cbRepContinua = new javax.swing.JCheckBox();
+        lblAlbum = new javax.swing.JLabel();
+        mbMenu = new javax.swing.JMenuBar();
+        mMenu = new javax.swing.JMenu();
+        mDades = new javax.swing.JMenu();
+        miCarregar = new javax.swing.JMenuItem();
+        miGuardar = new javax.swing.JMenuItem();
+        miSortir = new javax.swing.JMenuItem();
+        mInfo = new javax.swing.JMenu();
+        miSobre = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle(TITLE);
         setName(getClass().getSimpleName());
-        setPreferredSize(new java.awt.Dimension(640, 480));
+
+        cmbAlbum.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        lstAlbums.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        scpAlbums.setViewportView(lstAlbums);
+
+        lstBiblioteca.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        scpBiblioteca.setViewportView(lstBiblioteca);
+
+        lblBiblioteca.setText("Biblioteca");
+
+        lblAlbums.setText("Albums");
+
+        cbRepAleatoria.setText("Reproducció Aleatoria");
+        cbRepAleatoria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbRepAleatoriaActionPerformed(evt);
+            }
+        });
+
+        cbRepContinua.setText("Reproducció Continua");
+        cbRepContinua.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbRepContinuaActionPerformed(evt);
+            }
+        });
+
+        lblAlbum.setText("Album");
+
+        mMenu.setText("Menú");
+
+        mDades.setText("Dades");
+
+        miCarregar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.META_MASK));
+        miCarregar.setText("Carregar");
+        miCarregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miCarregarActionPerformed(evt);
+            }
+        });
+        mDades.add(miCarregar);
+
+        miGuardar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, java.awt.event.InputEvent.META_MASK));
+        miGuardar.setText("Guardar");
+        miGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miGuardarActionPerformed(evt);
+            }
+        });
+        mDades.add(miGuardar);
+
+        mMenu.add(mDades);
+
+        miSortir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.META_MASK));
+        miSortir.setText("Sortir");
+        mMenu.add(miSortir);
+
+        mbMenu.add(mMenu);
+
+        mInfo.setText("Info");
+
+        miSobre.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.META_MASK));
+        miSobre.setText("Sobre");
+        miSobre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miSobreActionPerformed(evt);
+            }
+        });
+        mInfo.add(miSobre);
+
+        mbMenu.add(mInfo);
+
+        setJMenuBar(mbMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 601, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblBiblioteca)
+                    .addComponent(scpBiblioteca, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cbRepAleatoria)
+                    .addComponent(cbRepContinua)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(scpAlbums, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cmbAlbum, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(8, 8, 8)
+                                .addComponent(lblAlbum))))
+                    .addComponent(lblAlbums))
+                .addContainerGap(262, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 334, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblBiblioteca)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(cbRepAleatoria)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cbRepContinua)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblAlbums)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(scpAlbums, javax.swing.GroupLayout.DEFAULT_SIZE, 346, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblAlbum)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cmbAlbum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addComponent(scpBiblioteca))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void miCarregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miCarregarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miCarregarActionPerformed
+
+    private void miGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miGuardarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miGuardarActionPerformed
+
+    private void miSobreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miSobreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miSobreActionPerformed
+
+    private void cbRepAleatoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbRepAleatoriaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbRepAleatoriaActionPerformed
+
+    private void cbRepContinuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbRepContinuaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbRepContinuaActionPerformed
 
     /**
      * Main
@@ -95,5 +252,23 @@ public class AplicacioUB4 extends JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox cbRepAleatoria;
+    private javax.swing.JCheckBox cbRepContinua;
+    private javax.swing.JComboBox<String> cmbAlbum;
+    private javax.swing.JLabel lblAlbum;
+    private javax.swing.JLabel lblAlbums;
+    private javax.swing.JLabel lblBiblioteca;
+    private javax.swing.JList<String> lstAlbums;
+    private javax.swing.JList<String> lstBiblioteca;
+    private javax.swing.JMenu mDades;
+    private javax.swing.JMenu mInfo;
+    private javax.swing.JMenu mMenu;
+    private javax.swing.JMenuBar mbMenu;
+    private javax.swing.JMenuItem miCarregar;
+    private javax.swing.JMenuItem miGuardar;
+    private javax.swing.JMenuItem miSobre;
+    private javax.swing.JMenuItem miSortir;
+    private javax.swing.JScrollPane scpAlbums;
+    private javax.swing.JScrollPane scpBiblioteca;
     // End of variables declaration//GEN-END:variables
 }
